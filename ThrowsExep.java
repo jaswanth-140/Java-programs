b572b9686570b9b9d888b169fae65082392fa38a
Jaswanth Reddy <2410030140@klh.edu.in>
2025-09-12 11:44:00 +0530

    Add files via upload

---
+++ ThrowsExep.java
@@ -0,0 +1,18 @@
+class ThrowsExep{
+    static void fun() throws IllegalAccessException
+    {
+        System.out.println("Inside fun().");
+        throw new IllegalAccessException("demo");
+    }
+    public static void main(String[]args)
+    {
+        try
+        {
+            fun();
+        }
+        catch(IllegalAccessException e)
+        {
+            System.out.println("Caught in main");
+        }
+    }
+}
