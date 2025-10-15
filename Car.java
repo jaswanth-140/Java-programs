95ffc8fcece168fe6ad1adc4452bf830615dd796
CHELLAMLLA JASWANTH REDDY <2410030140@klh.edu.in>
2025-10-15 20:42:53 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,16 @@
+class Car 
+{
+    static int totalCars = 0;
+    
+    public Car() 
+    {
+        totalCars++;
+    }
+    
+    public static void main(String[] args) 
+    {
+        new Car();
+        new Car();
+        System.out.println("Total cars: " + totalCars);
+    }
+}
